21b9e2408879916f33d814ba927136709aa988fe
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-07-16 10:08:12 -0500

    Add project files.

---
+++ BlazorSales.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorSales.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ BlazorSales/Server/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorSales.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorSales.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
