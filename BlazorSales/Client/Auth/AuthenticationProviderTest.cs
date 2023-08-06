5d9b2cdb1c258122f21b7ff3931c85afc1ee8fb6
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-08-06 08:18:19 -0500

    add security

---
+++ BlazorSales/Client/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorSales.Client.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //var anonimous = new ClaimsIdentity();
+            //return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimous)));
+            //var zuluUser = new ClaimsIdentity(authenticationType: "test");
+            //return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(zuluUser)));
+
+            var zuluUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Juan"),
+                new Claim("LastName", "Zulu"),
+                new Claim(ClaimTypes.Name, "zulu@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(zuluUser)));
+        }
+    }
+
+}
--- BlazorSales/Client/Program.cs
+++ BlazorSales/Client/Program.cs
@@ -1,6 +1,8 @@
 global using BlazorSales.Client.Repositories;
 using BlazorSales.Client;
+using BlazorSales.Client.Auth;
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,7 +11,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7084/") });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7084/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7084/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
+
 await builder.Build().RunAsync();
